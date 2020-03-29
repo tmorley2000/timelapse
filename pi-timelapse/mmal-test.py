777cfd7281292e85e475943955de3cb691d9f6db
Tim <tim@chaos.org.uk>
2020-03-29 19:05:40 +0100

    More mmal component tests

--- pi-timelapse/mmal-test.py
+++ pi-timelapse/mmal-test.py
@@ -4,13 +4,35 @@ from picamera import mmal, mmalobj as mo
 
 camera = mo.MMALCamera()
 
+print("Camera")
 for o in range(len(camera.outputs)):
- print("Output ",o)
+ print("Camera Output ",o)
  for a in camera.outputs[o].supported_formats :
-  print( hex(a), mmal.FOURCC_str(a) )
+  print( " ",hex(a), mmal.FOURCC_str(a) )
 
 resizer=mo.MMALISPResizer()
 
-print( len(resizer.outputs))
-for a in resizer.outputs[0].supported_formats :
- print( a, mmal.FOURCC_str(a))
+print("ISP Resizer") 
+for o in range(len(resizer.inputs)):
+ print("ISP Resizer Input ",o)
+ for a in resizer.inputs[o].supported_formats :
+  print( " ",hex(a), mmal.FOURCC_str(a))
+for o in range(len(resizer.outputs)):
+ print("ISP Resizer Output ",o)
+ for a in resizer.outputs[o].supported_formats :
+  print( " ",hex(a), mmal.FOURCC_str(a))
+
+
+encoder=mo.MMALImageEncoder()
+
+print("Image Encoder") 
+for o in range(len(encoder.inputs)):
+ print("Image Encoder Input ",o)
+ for a in encoder.inputs[o].supported_formats :
+  print( " ",hex(a), mmal.FOURCC_str(a))
+for o in range(len(encoder.outputs)):
+ print("Image Encoder Output ",o)
+ for a in encoder.outputs[o].supported_formats :
+  print( " ",hex(a), mmal.FOURCC_str(a))
+
+
